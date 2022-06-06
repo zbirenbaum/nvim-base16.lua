079f21db756f907dae99c1d83e0b256569fc1a6e
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-06-06 18:12:20 -0400

    add radium

---
+++ lua/hl_themes/radium.lua
@@ -0,0 +1,34 @@
+local colors = {
+   white = "#d4d4d5",
+   darker_black = "#0a0d11",
+   black = "#101317", --  nvim bg
+   black2 = "#191d22",
+   one_bg = "#212428",
+   one_bg2 = "#292c30",
+   one_bg3 = "#33363a",
+   grey = "#3e4145",
+   grey_fg = "#45484c",
+   grey_fg2 = "#4a4d51",
+   light_grey = "#525559",
+   red = "#f87070",
+   baby_pink = "#ff8e8e",
+   pink = "#ffa7a7",
+   line = "#30303a", -- for lines like vertsplit
+   green = "#37d99e",
+   vibrant_green = "#79dcaa",
+   blue = "#7ab0df",
+   nord_blue = "#87bdec",
+   yellow = "#ffe59e",
+   sun = "#ffeda6",
+   purple = "#c397d8",
+   dark_purple = "#b68acb",
+   teal = "#63b3ad",
+   orange = "#f0a988",
+   cyan = "#50cad2",
+   statusline_bg = "#15191e",
+   lightbg = "#24282d",
+   pmenu_bg = "#3bdda2",
+   folder_bg = "#5fb0fc",
+}
+
+return colors
